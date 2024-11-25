c172703db92200f233b75b9dc1e1596a31a03813
Coldboltage <alandreid@hotmail.co.uk>
2024-11-25 14:33:47 +0000

    Update local environment configuration and add Kubernetes resources for services

---
+++ env/local/testing/index.ts
@@ -0,0 +1,56 @@
+import { rabbitmqDeployment } from "../components/rabbitmq/rabbitmq-deployment";
+import { rabbitmqService } from "../components/rabbitmq/rabbitmq-service";
+import { rabbitmqConfigMap } from "../components/rabbitmq/rabbitmq-config";
+import { postgresDeployment } from "../components/postgres/postgres-deployment";
+import { postgresService } from "../components/postgres/postgres-service";
+import { postgresConfigMap } from "../components/postgres/postgres-config";
+import { postgresPVC } from "../components/postgres/postgres-pvc";
+import { postgresSecret } from "../components/postgres/postgres-secret";
+import { postgresPV } from "../components/postgres/postgres-pv";
+import { prometheusDeployment } from "../components/prometheus/prometheus-deployment";
+import { prometheusService } from "../components/prometheus/prometheus-service";
+import { prometheusConfigMap } from "../components/prometheus/prometheus-config";
+import { prometheusPV } from "../components/prometheus/prometheus-pv";
+import { prometheusPVC } from "../components/prometheus/prometheus-pvc";
+import { apiDeployment } from "../components/api/api-deployment";
+import { apiConfigMap } from "../components/api/api-config";
+import { apiService } from "../components/api/api-service";
+import { apiSecret } from "../components/api/api-secret";
+import { grafanaDeployment } from "../components/grafana/grafana-deployment";
+import { grafanaService } from "../components/grafana/grafana-service";
+import { grafanaPvc } from "../components/grafana/grafana-pvc";
+import { grafanaVolume } from "../components/grafana/grafana-volume";
+
+// RabitMQ resources
+export const rabbitmqDeploymentName = rabbitmqDeployment.metadata.name;
+export const rabbitmqServiceName = rabbitmqService.metadata.name;
+export const rabbitmqConfigMapName = rabbitmqConfigMap.metadata.name;
+
+// Postgres resources
+export const postgresDeploymentName = postgresDeployment.metadata.name;
+export const postgresServiceName = postgresService.metadata.name;
+export const postgresConfigMapName = postgresConfigMap.metadata.name;
+export const postgresPVCName = postgresPVC.metadata.name;
+export const postgresSecretName = postgresSecret.metadata.name;
+export const postgresPVName = postgresPV.metadata.name;
+
+// Prometheus resources
+export const prometheusDeploymentName = prometheusDeployment.metadata.name;
+export const prometheusServiceName = prometheusService.metadata.name;
+export const prometheusConfigMapName = prometheusConfigMap.metadata.name;
+export const prometheusPVName = prometheusPV.metadata.name;
+export const prometheusPVCName = prometheusPVC.metadata.name;
+
+// API resources
+export const apiDeploymentName = apiDeployment.metadata.name;
+export const apiServiceName = apiService.metadata.name;
+export const apiConfigMapName = apiConfigMap.metadata.name;
+export const apiSecretName = apiSecret.metadata.name
+
+// Grafana resources
+export const grafanaDeploymentName = grafanaDeployment.metadata.name;
+export const grafanaServiceName = grafanaService.metadata.name;
+export const grafanaPVCName = grafanaPvc.metadata.name;
+export const grafanaVolumeName = grafanaVolume.metadata.name;
+
+
--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
-
-
 import * as pulumi from '@pulumi/pulumi';
 
 const stack = pulumi.getStack();
 if (stack === 'local') {
   console.log("fired local")
-  require('./env/local/index');
+  require('./env/local/testing/index');
 } else if (stack === 'dev') {
   console.log("fired dev")
   require('./env/dev/index');
